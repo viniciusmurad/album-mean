dc566a452c6b182adf58cb0288544bff1c853010
Vinicius Murad <vm.murad@gmail.com>
2016-07-07 15:45:37 -0300

    added delete api

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,12 +1,28 @@
 var api = {};
 
-api.lista = function(req, res) {
-	var fotos = [
-		{_id: 1, titulo: 'Aurora boreal', url: 'https://www.oficinadanet.com.br/imagens/post/14596/aurora_nigruegahts_in_norway.jpg'},
-		{_id: 1, titulo: 'Aurora boreal 2', url: 'https://www.oficinadanet.com.br/imagens/post/14596/aurora_nigruegahts_in_norway.jpg'}
-	];
+var fotos = [
+	{_id: 1, titulo: 'Aurora boreal', url: 'https://www.oficinadanet.com.br/imagens/post/14596/aurora_nigruegahts_in_norway.jpg'},
+	{_id: 2, titulo: 'Aurora boreal 2', url: 'https://www.oficinadanet.com.br/imagens/post/14596/aurora_nigruegahts_in_norway.jpg'}
+];
 
+api.lista = function(req, res) {
 	res.json(fotos);
+}
+
+api.buscarId = function (req, res) {
+	var foto = fotos.find(function(foto) {
+		return foto._id == req.params.id;
+	});
+
+	res.json(foto);
+}
+
+api.deletarFoto = function (req,res) {
+	fotos.filter(function(foto) {
+		return foto._id != req.params.id;	
+	})
+
+	res.sendStatus(204);
 }
 
 module.exports = api;
--- app/routes/foto.js
+++ app/routes/foto.js
@@ -1,5 +1,10 @@
 module.exports = function(app) {
 	var api = app.api.foto;
 	app.get('/v1/fotos', api.lista);
-	app.get('/v1/fotos/:id', api.buscarId);
+	app.route('/v1/fotos/:id')
+		.get(api.buscarId)
+		.delete(api.deletarFoto);
+		
+	// app.get('/v1/fotos/:id', api.buscarId);
+	// app.delete('/v1/fotos/:id', api.deletarFoto);
 }
