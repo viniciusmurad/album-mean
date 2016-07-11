4de1ccfb2b87afb4c9d3a7216401fe93be410606
Vinicius Murad <vm.murad@gmail.com>
2016-07-11 14:56:49 -0300

    update api

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 var api = {};
+var model = mongoose.model('Foto');
 
 api.lista = function(req, res) {
-	var model = mongoose.model('Foto');
 	model.find({})
 		.then(function (fotos) {
 			res.json(fotos);
@@ -13,19 +13,44 @@ api.lista = function(req, res) {
 }
 
 api.buscarId = function (req, res) {
-	
+	model.findById(req.params.id)
+		.then(function (foto) {
+			if(!foto) throw Error('Foto não encontrada');
+			res.json(foto);
+		}, function (error) {
+			console.log(error);
+			res.status(404).json(error);
+		})
 }
 
 api.deletarFoto = function (req, res) {
-	
+	model.remove({_id: req.params.id})
+		then(function() {
+			res.sendStatus(204);
+		}, function (error) {
+			console.log(error);
+			res.status(500).json(error);
+		})
 }
 
 api.addFoto = function (req, res) {
-	
+	model.create(req.body)
+		.then(function (foto) {
+			res.json(foto);
+		}, function (error) {
+			console.log(error);
+			res.status(500).json(error);
+		})
 }
 
 api.atualizarFoto = function (req, res) {
-	
+	model.findByIdAndUpdate(req.params.id, req.body)
+		.then(function (foto) {
+			res.json(foto);
+		}, function(error) {
+			console.log(error);
+			res.status(500).json(error);
+		})
 }
 
 module.exports = api;
