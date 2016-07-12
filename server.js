c595bd9ff3ba9a49bfae5a9f5ffcfe0ceeab0828
Vinicius Murad <vm.murad@gmail.com>
2016-07-12 15:07:36 -0300

    modified path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
-require('./config/database')('localhost/albummean');
+require('./config/database')('localhost/albummea');
 
 http.createServer(app).listen(3000, function() {
 	console.log('Servidor funcionando');
